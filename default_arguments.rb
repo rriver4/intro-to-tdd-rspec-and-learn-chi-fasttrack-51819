5d3a151fd175bd84b22f93b2b6d04b6cf9f592cc
loyal-recipe-0842 <riveraronirae@gmail.com>
2019-06-10 00:18:03 +0000

    Automatically backed up by Learn

--- default_arguments.rb
+++ default_arguments.rb
@@ -1,3 +1,9 @@
 def greeting(name, language="Ruby")
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
+
+
+greeting("Sophie", "Ember.js")
+# > Hello, Sophie. We heard you are a great Ember.js programmer.
+
+greeting("Dan")
